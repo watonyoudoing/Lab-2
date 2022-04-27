9b0d788431a0a3d7396e335308e2d6daa6920aff
{wang <{wangwangoppa@gmail.com}>
2022-04-27 14:25:36 +0800

    nice

--- lab2.py
+++ lab2.py
@@ -1,1 +1,46 @@
-print("hello world")
+
+
+
+
+def display_main_menu():
+    print ("this is america")
+
+def get_user_input():
+       x= input(" Enter the number of element ")
+       y = x.split(",")
+       y= list(map(int, y))
+       print(y)
+       return(y)
+def calc_average_temperature(Temp):
+    Total=sum(Temp)
+    print(Total)
+
+def calc_min_max_temperature(Temp):
+    biggest= Temp[0]
+    smallest= Temp[0]
+    for i in range(len(Temp)):
+        if biggest<Temp[i]:
+            biggest=Temp[i]
+        elif smallest>Temp[i]:
+            smallest=Temp[i]
+    return(biggest,smallest)
+
+
+
+
+def main():
+    print("xd")
+    display_main_menu()
+    Temp = get_user_input()
+    calc_average_temperature(Temp)
+    print(calc_min_max_temperature(Temp))
+if __name__ == "__main__":
+  main()
+
+
+
+
+
+
+
+
